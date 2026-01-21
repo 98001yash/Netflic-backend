1aa463f46dd9df3ffcf5d51a14b45b687b8da026
98001yash <yashchauhan.gaya@gmail.com>
2026-01-21 16:54:12 +0530

    added the discovery-server

---
+++ Discovery-server/src/test/java/com/company/BingeBox_backend_application/Discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.BingeBox_backend_application.Discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
